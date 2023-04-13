ae29769a779dcabe1e2fa05cc58148382f981f04
Karem Sobhy <karemsobhy020@gmail.com>
2023-04-13 15:21:58 +0200

    add auth guards on routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
+import {useUserStore} from "../store/user_store"
 import MainLayout from '../layout/MainLayout.vue'
+
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,13 +28,29 @@ const router = createRouter({
       path: '/login',
       name: 'Login',
       component: () => import('../pages/Login.vue'),
+      meta: {
+        requiresUnauth: true
+      }
     },
     {
       path: '/register',
       name: 'Register',
       component: () => import('../pages/Register.vue'),
+      meta: {
+        requiresUnauth: true
+      }
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  const token= useUserStore().token;
+  // console.log(token);
+  if (to.meta.requiresAuth && !token) {
+      next('/login');
+    }
+  if (to.meta.requiresUnauth && token) {
+      next('/');
+    }
+      next();
+});
 export default router
