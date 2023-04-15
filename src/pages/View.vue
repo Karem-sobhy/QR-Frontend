eefa7184e95ba41a7117cbc25b2227d2cf316bb8
Karem Sobhy <karemsobhy020@gmail.com>
2023-04-16 00:07:39 +0200

    view done

--- src/pages/View.vue
+++ src/pages/View.vue
@@ -20,16 +20,33 @@
     <div class="content">
       <div class="container-fluid">
         <div class="row justify-content-center">
-          <div class="col-12 col-md-10 col-lg-8">
-            <div class="custom-file">
-             
-                <label class="custom-file-label" for="customFile"></label>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">DataTable with default features</h3>
+              </div>
+
+              <div class="card-body">
+                <DataTable
+                        :options="options"
+                        class="table table-bordered table-striped"
+                      >
+                        <thead>
+                          <tr>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>SSN</th>
+                            <th>Test1</th>
+                            <th>Test2</th>
+                            <th>Test3</th>
+                            <th>Test4</th>
+                            <th>Final</th>
+                            <th>Grade</th>
+                          </tr>
+                        </thead>
+                      </DataTable>
+              </div>
             </div>
-            <!-- <div class="">
-                <input @change="fileChanged" type="file" >
-                <label class="custom-file-label" for="customFile">{{ file }}</label>
-            </div> -->
-            
           </div>
           <!-- /.col -->
         </div>
@@ -41,3 +58,44 @@
   </div>
   <!-- /.content-wrapper -->
 </template>
+<script setup>
+import DataTable from "datatables.net-vue3";
+import DataTablesLib from "datatables.net-bs4";
+
+DataTable.use(DataTablesLib);
+</script>
+<script>
+import { useUserStore } from "../store/user_store";
+export default {
+  data() {
+    return {
+      // token:useUserStore().token,
+      options: {
+        processing: true,
+        serverSide: true,
+        autoWidth: true,
+        ajax: {
+          url: "http://127.0.0.1:8000/api/view",
+          headers: { Authorization: "Bearer " + useUserStore().token },
+        },
+        columns: [
+          { data: "last_name" },
+          { data: "first_name" },
+          { data: "ssn" },
+          { data: "test1", searchable: false },
+          { data: "test2", searchable: false },
+          { data: "test3", searchable: false },
+          { data: "test4", searchable: false },
+          { data: "final", searchable: false },
+          { data: "grade" },
+        ],
+      },
+    };
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+<style>
+@import 'datatables.net-bs4'
+</style>
